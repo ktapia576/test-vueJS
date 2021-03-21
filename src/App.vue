95dca0a58a750f9ca30568eb5658f067e48eda97
ktapia576 <ktapia576@hotmail.com>
2021-03-20 20:27:37 -0400

    Add my own todoList component with prop greeting being passed to it

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld v-bind:msg="75+1" />
+  <todoList greeting="HELLO"/>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import todoList from './components/todoList.vue';
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
---
+++ src/components/todoList.vue
@@ -0,0 +1,40 @@
+<template>
+    <p>{{ greeting }}</p>
+    <ol id=todoList>
+        <li v-for="todo in todos">
+        {{ todo.text }}
+        </li>
+    </ol>
+</template>
+
+<script>
+
+export default {
+    props: {
+        greeting: String,
+    },
+    data() {
+        return {
+            todos: [
+                { id: 0, text: 'Vegetables' },
+                { id: 1, text: 'Cheese' },
+                { id: 2, text: 'Whatever else humans are supposed to eat' }
+            ]
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+#todoList {
+    margin: auto;
+    width: 50%;
+    border: 3px solid green;
+    padding: 30px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
