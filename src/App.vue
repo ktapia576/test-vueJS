1d1dd521ae12d95537dbdb707f91fb137b266ab3
ktapia576 <ktapia576@hotmail.com>
2021-03-20 21:13:04 -0400

    Remove experimental script setup and change app component

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,17 @@
   <todoList greeting="HELLO"/>
 </template>
 
-<script setup>
+<script>
 import HelloWorld from './components/HelloWorld.vue';
 import todoList from './components/todoList.vue';
+
+
+export default {
+  components: {
+    HelloWorld,
+    todoList
+  }
+}
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
